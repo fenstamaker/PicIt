a4defde5a2f2c85dd61eab859a8f6913fc1f2496
Gary Fenstamaker <fenstamaker@gmail.com>
2012-07-09 21:23:19 -0400

    Added an interpreter for the tree DOT file

--- src/edu/montclair/hci/picit/ml/DecisionNode.java
+++ src/edu/montclair/hci/picit/ml/DecisionNode.java
@@ -10,25 +10,94 @@ public class DecisionNode {
 	private DecisionNode right;
 	private boolean isLeaf;
 	private String classification;
+	private String attr;
+	private String operation;
 	
-	public DecisionNode(String name)
-	{
-		
+	public DecisionNode(String name) {
+		this.name = name;
 	}
 	
-	public DecisionNode(String lineFromFile, HashMap<String, DecisionNode> otherNodes)
-	{
-		// parse the node
+	public String expand(int r, int g, int b) {
+		if (!isLeaf) {
+			
+			if ( attr.equals("red") ) {
+				
+				if ( operation.equals("<") ) {
+					if ( r < threshold ) {
+						return right.expand(r, g, b);
+					} else {
+						return left.expand(r, g, b);
+					}
+				} else {
+					if ( r > threshold ) {
+						return right.expand(r, g, b);
+					} else {
+						return left.expand(r, g, b);
+					}
+				}
+				
+			} else if ( attr.equals("green") ) {
+				
+				if ( operation.equals("<") ) {
+					if ( g < threshold ) {
+						return right.expand(r, g, b);
+					} else {
+						return left.expand(r, g, b);
+					}
+				} else {
+					if ( g > threshold ) {
+						return right.expand(r, g, b);
+					} else {
+						return left.expand(r, g, b);
+					}
+				}
+				
+			} else if ( attr.equals("blue") ) {
+				
+				if ( operation.equals("<") ) {
+					if ( b < threshold ) {
+						return right.expand(r, g, b);
+					} else {
+						return left.expand(r, g, b);
+					}
+				} else {
+					if ( b > threshold ) {
+						return right.expand(r, g, b);
+					} else {
+						return left.expand(r, g, b);
+					}
+				}
+				
+			}
+			
+		}
 		
-		// get name
-		
-		otherNodes.add(name, this);
-		
+		return classification;
 	}
 	
-	public void setAttributes(String fileLine)
-	{
-		
+	public void setRightNode(DecisionNode node) {
+		right = node;
+	}
+	
+	public void setLeftNode(DecisionNode node) {
+		left = node;
+	}
+	
+	public void setAsLeaf(String classification) {
+		isLeaf = true;
+		this.classification = classification;
+	}
+	
+	public void setAttribute(String attr) {
+		this.attr = attr;
+	}
+	
+	public void setThreshold(float value) {
+		threshold = value;
+	}
+	
+	public void setOperation(String op) {
+		operation = op;
 	}
 	
 }
--- src/edu/montclair/hci/picit/ml/DecisionTree.java
+++ src/edu/montclair/hci/picit/ml/DecisionTree.java
@@ -24,35 +24,114 @@ public class DecisionTree {
 			// hashmap of all decision nodes
 			HashMap<String, DecisionNode> nodes = new HashMap<String, DecisionNode>();
 			
+			// Root Node
+			
 			String rootText = file.readLine();
 			
-			root = new DecisionNode(rootText, nodes);
+			String rootName = rootText.split("@")[1].split("\"")[0];
+			
+			root = new DecisionNode(rootName);
+			nodes.put(rootName, root);
+			
+			String rootAttribute = rootText.split("label=\"")[1].split("[ ]<")[0].trim();
+			String rootOperation = rootText.split("label=\"[a-z]+")[1].split("[0-9]+")[0].trim();
+			float rootValue = Float.parseFloat( rootText.split("[<>][ ]")[1].split("[ ][(]score")[0].trim() );
 
+			root.setAttribute(rootAttribute);
+			root.setOperation(rootOperation);
+			root.setThreshold(rootValue);
+			
+			// Root's First Child
+			
+			rootText = file.readLine();
+
+			String rootChild = rootText.split("@")[2].split("\"")[0];
+			
+			DecisionNode rootChildNode = new DecisionNode(rootChild);
+			root.setLeftNode(rootChildNode);
+			nodes.put(rootChild, rootChildNode);
+			
+			// Root's Second Child
+			
+			rootText = file.readLine();
+
+			rootChild = rootText.split("@")[2].split("\"")[0];
+			
+			rootChildNode = new DecisionNode(rootChild);
+			root.setRightNode(rootChildNode);
+			nodes.put(rootChild, rootChildNode);
+			
+			// All other Nodes
 			String text = file.readLine();
-			// read through file and create nodes
 			while ( text != null )
 			{
-				String[] parts = text.split("@");
-				String name;
-				if ( parts.length < 3 )
-					name = parts[1].split("\"")[0];
-				else 
-					name = parts[2].split("\"")[0];
+				String nodeType = text.split("[.]")[6].split("@")[0];
 				
-				DecisionNode dn = new DecisionNode(name);
-				nodes.put(name, dn);
+				if ( nodeType.equals("ScoreTestNode") ) {
+					String nodeName = text.split("@")[1].split("\"")[0];
+					DecisionNode node;
+					
+					if ( nodes.containsKey(nodeName) ) {
+						node = nodes.get(nodeName);
+					} else { 
+						node = new DecisionNode(nodeName);
+						nodes.put(nodeName, node);
+					}
+					
+					String attribute = text.split("label=\"")[1].split("[ ]<")[0].trim();
+					String operation = text.split("label=\"[a-z]+")[1].split("[0-9]+")[0].trim();
+					float value = Float.parseFloat( text.split("[<>][ ]")[1].split("[ ][(]score")[0].trim() );
+					
+					node.setAttribute(attribute);
+					node.setOperation(operation);
+					node.setThreshold(value);
+					
+					// Root's First Child
+					
+					text = file.readLine();
+
+					String nodeChild = text.split("@")[2].split("\"")[0];
+					
+					DecisionNode child = new DecisionNode(nodeChild);
+					node.setLeftNode(child);					
+					nodes.put(nodeChild, child);
+					
+					// Root's Second Child
+					
+					text = file.readLine();
+					nodeChild = text.split("@")[2].split("\"")[0];
+					
+					child = new DecisionNode(nodeChild);
+					node.setRightNode(child);
+					nodes.put(nodeChild, child);
+				} else if ( nodeType.equals("LeafNode") ) {
+					String nodeName = text.split("@")[1].split("\"")[0];
+					DecisionNode node;
+					
+					if ( nodes.containsKey(nodeName) ) {
+						node = nodes.get(nodeName);
+					} else { 
+						node = new DecisionNode(nodeName);
+						nodes.put(nodeName, node);
+					}
+
+					String classification = text.split("-[ ]")[1].split("Weight")[0].trim();
+
+					node.setAsLeaf(classification);
+					
+				}
 				
 				text = file.readLine();
 			}
 		} catch (FileNotFoundException e) {
-			Log.e("DECISIONTREE", "Could not find tree file");
 			e.printStackTrace();
 		} catch (IOException e) {
-			Log.e("DECISIONTREE", "IO Error when reading line");
 			e.printStackTrace();
 		}
 		
 		
+		String result = root.expand(100, 100, 100);
+		System.out.println(result);
 				
 	}
 	
